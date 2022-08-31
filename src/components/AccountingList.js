b4f60a514279e58031602f91a2ee95641bccbc23
Rivet-NG <kjuwah@lions.molloy.edu>
2022-08-31 08:32:56 -0400

    Assignment list updated!

--- src/components/AccountingList.js
+++ src/components/AccountingList.js
@@ -1,30 +1,30 @@
 export const AccountingList = [
   {
     assignmentTitle: "Accounting 1",
-    dueDate: "Aug 15, 2022 23:59:59"
+    dueDate: "Sep 1, 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 2",
-    dueDate: "Aug 16, 2022 23:59:59"
+    dueDate: "Sep 2, 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 3",
-    dueDate: "Aug 17, 2022 23:59:59"
+    dueDate: "Sep 3, 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 4",
-    dueDate: "Aug 18, 2022 23:59:59"
+    dueDate: "Sep 4, 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 5",
-    dueDate: "Aug 19, 2022 23:59:59"
+    dueDate: "Sep 5, 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 6",
-    dueDate: "Aug 20, 2022 23:59:59"
+    dueDate: "Sep 6, 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 7",
-    dueDate: "Aug 21, 2022 23:59:59"
+    dueDate: "Sep 7, 2022 23:59:59"
   }
 ];
