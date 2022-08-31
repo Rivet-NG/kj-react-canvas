9ca60d9641fc2d3696664d8b778a5dad6356515f
Rivet-NG <43015084+Rivet-NG@users.noreply.github.com>
2022-08-30 23:44:17 -0400

    ColorList updated!

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,18 +1,18 @@
 export const ColorList = [
   {
     assignmentTitle: "Color 1",
-    dueDate: "Sep 8, 2022 23:59:59"
+    dueDate: "Sep 8 2022 23:59:59"
   },
   {
     assignmentTitle: "Color 2",
-    dueDate: "Sep 9, 2022 23:59:59"
+    dueDate: "Sep 9 2022 23:59:59"
   },
   {
     assignmentTitle: "Color 3",
-    dueDate: "Sep 10, 2022 23:59:59"
+    dueDate: "Sep 10 2022 23:59:59"
   },
   {
     assignmentTitle: "Color 4",
-    dueDate: "Sep 11, 2022 23:59:59"
+    dueDate: "Sep 11 2022 23:59:59"
   }
 ];
