3d14cada9549396186969083da2d47f0c6cac929
Rivet-NG <43015084+Rivet-NG@users.noreply.github.com>
2022-08-30 23:43:22 -0400

    AccountingList updated!

--- src/components/AccountingList.js
+++ src/components/AccountingList.js
@@ -1,30 +1,30 @@
 export const AccountingList = [
   {
     assignmentTitle: "Accounting 1",
-    dueDate: "Sep 1, 2022 23:59:59"
+    dueDate: "Sep 1 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 2",
-    dueDate: "Sep 2, 2022 23:59:59"
+    dueDate: "Sep 2 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 3",
-    dueDate: "Sep 3, 2022 23:59:59"
+    dueDate: "Sep 3 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 4",
-    dueDate: "Sep 4, 2022 23:59:59"
+    dueDate: "Sep 4 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 5",
-    dueDate: "Sep 5, 2022 23:59:59"
+    dueDate: "Sep 5 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 6",
-    dueDate: "Sep 6, 2022 23:59:59"
+    dueDate: "Sep 6 2022 23:59:59"
   },
   {
     assignmentTitle: "Accounting 7",
-    dueDate: "Sep 7, 2022 23:59:59"
+    dueDate: "Sep 7 2022 23:59:59"
   }
 ];
